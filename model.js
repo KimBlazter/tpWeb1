abf4ac7cd419f83a62f729716fc7e33e6b88f8b3
Martin Saunier <martin.saunier@etudiant.univ-rennes.fr>
2025-10-12 20:24:22 +0200

    implement Drawing and shape classes

--- model.js
+++ model.js
@@ -1,3 +1,48 @@
-
 // Implémenter ici les 4 classes du modèle.
 // N'oubliez pas l'héritage !
+
+class Drawing {
+    constructor() {
+        this.shapes = [];
+    }
+
+    addShape(shape) {
+        this.shapes.push(shape);
+    }
+
+    getShapes() {
+        return this.shapes;
+    }
+}
+
+class Shape {
+    constructor(color, thickness) {
+        this.color = color || 'black';
+        this.thickness = thickness || 1;
+    }
+}
+
+class Rectangle extends Shape {
+    constructor(origin, width, height, color, thickness) {
+        super(color, thickness);
+        this.origin = origin;
+        this.width = width;
+        this.height = height;
+    }
+}
+
+class Line extends Shape {
+    constructor(startPoint, endPoint, color, thickness) {
+        super(color, thickness);
+        this.startPoint = startPoint;
+        this.endPoint = endPoint;
+    }
+}
+
+class Circle extends Shape {
+    constructor(center, radius, color, thickness) {
+        super(color, thickness);
+        this.center = center;
+        this.radius = radius;
+    }
+}
